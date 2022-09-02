5333721b6733ce99ab86d5576ef4b674c7bc338a
eun <eun@hexlant.com>
2022-09-02 14:13:36 +0900

    .

--- src/test/ethereum/erc20/main.test.ts
+++ src/test/ethereum/erc20/main.test.ts
@@ -1,7 +1,6 @@
 import { describe, test } from "@jest/globals";
 import { erc20 } from "../../../generate/api";
 import { buildERC20 } from "../../../material/ethereum/erc20/erc20";
-import { zipContract } from "../../../utils/zip";
 
 describe("ERC20", () => {
   test("generate", () => {
