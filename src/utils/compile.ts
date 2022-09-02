44fb7d42ad483935f99c222ad8778a73669f4f45
eun <eun@hexlant.com>
2022-09-02 14:47:21 +0900

    .

--- src/utils/compile.ts
+++ src/utils/compile.ts
@@ -1,4 +1,4 @@
-import * as fs from "fs";
+import fs from "fs";
 import solc from "solc";
 
 export const compile = (contractName: string, content: string) => {
--- src/utils/helpers/variables.ts
+++ src/utils/helpers/variables.ts
@@ -1,5 +1,5 @@
 import path from "path";
-import * as fs from "fs";
+import fs from "fs";
 import * as constants from "./constants";
 import { existsFile } from "./file";
 
