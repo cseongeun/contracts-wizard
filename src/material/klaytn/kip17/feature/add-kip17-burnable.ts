36c84dd160f896dcd76c52cd4b85f8e5892e65e4
eun <eun@hexlant.com>
2022-09-05 14:40:51 +0900

    .

--- src/material/klaytn/kip17/feature/add-kip17-burnable.ts
+++ src/material/klaytn/kip17/feature/add-kip17-burnable.ts
@@ -4,6 +4,6 @@ import { pathPrefix } from "../../../../utils/sourcecode";
 export function addKIP17Burnable(c: ContractBuilder) {
   c.addParent({
     name: "KIP17Burnable",
-    path: `${pathPrefix}/ethereum/kip17/features/KIP17Burnable.sol`,
+    path: `${pathPrefix}/klaytn/kip17/features/KIP17Burnable.sol`,
   });
 }
